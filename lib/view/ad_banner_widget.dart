b4edeb5317982955937507a4a890ce6b32d523cb
Zero <1300326388@qq.com>
2021-09-01 22:08:02 +0800

    1、更新版本号 2、增加 Banner 广告说明

--- lib/view/ad_banner_widget.dart
+++ lib/view/ad_banner_widget.dart
@@ -14,7 +14,7 @@ class AdBannerWidget extends StatefulWidget {
   }) : super(key: key);
   // 广告 id
   final String posId;
-  // 广告刷新间隔,[30~120]之间的数字，单位为 s,默认30S
+  // 广告刷新间隔，0 或[30~120]之间的数字，单位为 s,默认 30s
   // Android:0 表示不自动轮播
   // iOS:0 表示关闭轮播动画，因为 iOS 没有不轮播
   final int interval;
