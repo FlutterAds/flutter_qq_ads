a2b161788d030ddc6481e9c3709e450ac1a3f4cb
Zero <1300326388@qq.com>
2021-12-04 10:56:19 +0800

    1、优化处理命名冲突问题

--- ios/Classes/Load/FAQFeedAdManager.m
+++ ios/Classes/Load/FAQFeedAdManager.m
@@ -10,27 +10,27 @@
 @implementation FAQFeedAdManager
 
 static FAQFeedAdManager *adManager;// 广告管理实例
-NSMutableDictionary *adList;// 已加载信息流广告列表
+NSMutableDictionary *faqAdList;// 已加载信息流广告列表
 
 + (instancetype)share{
     dispatch_once_t once_token;
     dispatch_once(&once_token, ^{
-        adList = [[NSMutableDictionary alloc] init];
+        faqAdList = [[NSMutableDictionary alloc] init];
         adManager=[[FAQFeedAdManager alloc] init];
     });
     return adManager;
 }
 
 - (void)putAd:(NSNumber *)key value:(GDTNativeExpressAdView *)value{
-    [adList setObject:value forKey:key];
+    [faqAdList setObject:value forKey:key];
 }
 
 - (GDTNativeExpressAdView *)getAd:(NSNumber *)key{
-    return [adList objectForKey:key];
+    return [faqAdList objectForKey:key];
 }
 
 - (void)removeAd:(NSNumber *)key{
-    [adList removeObjectForKey:key];
+    [faqAdList removeObjectForKey:key];
 }
 
 @end
